44443541d79f2a7420c2244b93d6291a54f1c149
CarloBelfor <carlo@belfor-it.nl>
2022-05-13 09:39:31 +0200

    C++ HDF5 database

--- backtester.py
+++ backtester.py
@@ -7,14 +7,11 @@ import strategies.support_resistance
 
 
 def run(exchange: str, symbol: str, strategy: str, tf: str, from_time: int, to_time: int):
-    
+
     params_des = STRAT_PARAMS[strategy]
 
     params = dict()
-    """
-    p_name is short for parameter name example: "MA Period"
-    p is short for parameter example:{"name": "MA Period", "type": int}
-    """
+
     for p_code, p in params_des.items():
         while True:
             try:
@@ -28,34 +25,39 @@ def run(exchange: str, symbol: str, strategy: str, tf: str, from_time: int, to_t
         data = h5_db.get_data(symbol, from_time, to_time)
         data = resample_timeframe(data, tf)
 
-        pnl = strategies.obv.backtest(data, ma_period=params["ma_period"])
-    # default 9 as moving average period
-        #print(strategies.obv.backtest(data, 9))
+        pnl, max_drawdown = strategies.obv.backtest(data, ma_period=params["ma_period"])
 
-        return pnl
-    
+        return pnl, max_drawdown
+
     elif strategy == "ichimoku":
         h5_db = Hdf5Client(exchange)
         data = h5_db.get_data(symbol, from_time, to_time)
         data = resample_timeframe(data, tf)
 
-        pnl = strategies.ichimoku.backtest(data, tenkan_period=params["tenkan"], kijun_period=params["kijun"])
+        pnl, max_drawdown = strategies.ichimoku.backtest(data, tenkan_period=params["tenkan"], kijun_period=params["kijun"])
 
-        #print(strategies.ichimoku.backtest(data, tenkan_period=9, kijun_period=26))
-        return pnl
+        return pnl, max_drawdown
 
     elif strategy == "sup_res":
         h5_db = Hdf5Client(exchange)
         data = h5_db.get_data(symbol, from_time, to_time)
         data = resample_timeframe(data, tf)
 
-        pnl = strategies.support_resistance.backtest(data, min_points=params["min_points"],
-                                                     min_diff_points=params["min_diff_points"],
-                                                     rounding_nb=params["rounding_nb"],
+        pnl, max_drawdown = strategies.support_resistance.backtest(data, min_points=params["min_points"],
+                                                                 min_diff_points=params["min_diff_points"],
+                                                                 rounding_nb=params["rounding_nb"],
                                                      take_profit=params["take_profit"], stop_loss=params["stop_loss"])
-        
-        #print(strategies.support_resistance.backtest(data, min_points=3, min_diff_points=7, rounding_nb=400,take_profit=3, stop_loss=3))
 
-        return pnl
+        # pnl = "pnl " + str(round(pnl, 2)) + " %"
+        # max_drawdown = str(round(max_drawdown, 2))
+        return pnl, max_drawdown
 
-        
+
+    """
+    p_name is short for parameter name example: "MA Period"
+    p is short for parameter example:{"name": "MA Period", "type": int}
+    #print(strategies.support_resistance.backtest(data, min_points=3, min_diff_points=7, rounding_nb=400,take_profit=3, stop_loss=3))
+    #print(strategies.ichimoku.backtest(data, tenkan_period=9, kijun_period=26))
+        # default 9 as moving average period
+        #print(strategies.obv.backtest(data, 9))
+    """
---
+++ readme.txt
@@ -0,0 +1,14 @@
+install plugins
+Cmake
+cmake tools
+C/C++
+    :cpp on 17
+
+Download for windows
+https://github.com/Kitware/CMake/releases/download/v3.23.1/cmake-3.23.1-windows-x86_64.msi
+
+install GCC for windows compiler
+www.msys2.org/download/
+
+download https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.12/hdf5-1.12.2/bin/windows/
+1.12 (zip file)
